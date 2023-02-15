eb5abc49b2904e6eeb00d6e80916dda34f726a6d
Allan RIbeiro <35040806+AllanPazRibeiro@users.noreply.github.com>
2023-02-15 16:45:54 -0300

    fix: fixing the auth endpoint url (#8)

--- lib/UrlHelper.ts
+++ lib/UrlHelper.ts
@@ -1,15 +1,32 @@
 import { IAppAccessors } from "@rocket.chat/apps-engine/definition/accessors";
 import { IApiEndpointMetadata } from "@rocket.chat/apps-engine/definition/api";
-import { AuthenticationScopes, getMicrosoftAuthorizeUrl, TestEnvironment } from "./Const";
+import {
+    AuthenticationScopes,
+    getMicrosoftAuthorizeUrl,
+    TestEnvironment,
+} from "./Const";
 
-export const getRocketChatAppEndpointUrl = async (appAccessors: IAppAccessors, appEndpointPath: string) : Promise<string> => {
+export const getRocketChatAppEndpointUrl = async (
+    appAccessors: IAppAccessors,
+    appEndpointPath: string
+): Promise<string> => {
+    const webhookEndpoint: IApiEndpointMetadata =
+        appAccessors.providedApiEndpoints.find(
+            (endpoint) => endpoint.path === appEndpointPath
+        ) as IApiEndpointMetadata;
+    let siteUrl: string = await appAccessors.environmentReader
+        .getServerSettings()
+        .getValueById("Site_Url");
 
-    const webhookEndpoint: IApiEndpointMetadata = appAccessors.providedApiEndpoints
-        .find((endpoint) => endpoint.path === appEndpointPath) as IApiEndpointMetadata;
-    let siteUrl: string = await appAccessors.environmentReader.getServerSettings().getValueById('Site_Url');
-    siteUrl = siteUrl.substring(0, siteUrl.length - 1);
-    
-    if (TestEnvironment.tunnelServiceUrl && TestEnvironment.tunnelServiceUrl !== '') {
+    siteUrl =
+        siteUrl.slice(-1) === "/"
+            ? siteUrl.substring(0, siteUrl.length - 1)
+            : siteUrl;
+
+    if (
+        TestEnvironment.tunnelServiceUrl &&
+        TestEnvironment.tunnelServiceUrl !== ""
+    ) {
         siteUrl = TestEnvironment.tunnelServiceUrl;
     }
 
@@ -20,13 +37,14 @@ export const getLoginUrl = (
     aadTenantId: string,
     aadClientId: string,
     authEndpointUrl: string,
-    userId: string): string => {
+    userId: string
+): string => {
     let url = getMicrosoftAuthorizeUrl(aadTenantId);
     url += `?client_id=${aadClientId}`;
-    url += '&response_type=code';
+    url += "&response_type=code";
     url += `&redirect_uri=${authEndpointUrl}`;
-    url += '&response_mode=query';
-    url += `&scope=${AuthenticationScopes.join('%20')}`;
+    url += "&response_mode=query";
+    url += `&scope=${AuthenticationScopes.join("%20")}`;
     url += `&state=${userId}`;
 
     return url;
